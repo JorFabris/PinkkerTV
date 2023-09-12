a72ff8ecb34223c7a76f3447acd1c8feb033a007
JorFabris <jfabris@reba.com.ar>
2023-09-12 11:05:34 -0300

    New aliases added and logger for reactotron

---
+++ src/utils/Logger.ts
@@ -0,0 +1,41 @@
+import Reactotron from 'reactotron-react-native';
+
+export default class Logger {
+    static log = (message = 'Log', ...args: any) => {
+        console.log(typeof message);
+
+        if (typeof message !== 'string') {
+            throw new Error('Message has to be a string');
+        }
+        if (__DEV__) {
+            Reactotron.display({
+                name: 'LOG',
+                preview: message,
+                value: { message, args },
+                important: true,
+            });
+        }
+    };
+
+    static warn(message: string, ...args: any) {
+        if (__DEV__) {
+            Reactotron.display({
+                name: 'WARN',
+                preview: message,
+                value: { message, args },
+                important: true,
+            });
+        }
+    }
+
+    static error(message: string, report: any) {
+        if (__DEV__) {
+            Reactotron.display({
+                name: 'ERROR',
+                preview: message,
+                value: { message, ...report },
+                important: true,
+            });
+        }
+    }
+}
